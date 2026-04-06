483c0ca0f5f71a7e5ff782086dad3beb0fc4e990
tagor1231 <tagorkumarbasunia@gmail.com>
2026-04-06 13:23:04 +0600

    fix api baseURL to render backend

--- backend/server.py
+++ backend/server.py
@@ -1002,6 +1002,7 @@ async def get_all_users(current_user: dict = Depends(get_current_user)):
         raise HTTPException(status_code=403, detail="Admin access required")
 
     users = await db.users.find({}, {"_id": 0, "password_hash": 0}).to_list(1000)
+    print("USERS FROM DB =", users)
     return users
 
 
